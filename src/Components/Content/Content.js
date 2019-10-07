9559a03212bea43ac9052896384ce14af012ecf5
Kirill Orlov <orlovkirill31@mail.ru>
2019-10-07 21:37:40 +0300

    create layout

---
+++ src/Components/Content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './SIdebar.module.css';
+
+const Sidebar = () => {
+    return (
+        <aside>SIDEBAR</aside>);
+}
+
+export default Sidebar;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.module.css';
+
+function App() {
+    return (
+        <header>HEADER</header>);
+}
+
+export default App;
