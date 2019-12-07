df913d5f950b46cbbbcd1412958d7337b8d09392
Kirill Orlov <orlovkirill31@mail.ru>
2019-12-07 12:38:25 +0300

    auth0 init trying

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React, {useState} from 'react';
 import s from './App.module.css';
 import WidgetContainer from "./Components/Widget/WidgetContainer";
-import {useHistory} from "react-router-dom";
+import {Route, Router, Switch, useHistory} from "react-router-dom";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Content from "./Components/Content/Content";
 import RootWithSlideWidget from 'react-sidebar';
 import Login from "./Components/Login/Login";
+import NavBar from "./Components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+import Profile from "./Components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./Components/PrivateRoute";
 
 
 const App = (props) => {
 
+    const { loading } = useAuth0();
+
     const [isWidgetShown, setIsWidgetShown] = useState(false);
     const pathHistory = useHistory();
     const isAuth = Math.random() > 0.5;
-   // if (isAuth) pathHistory.push('/login');
+    // if (isAuth) pathHistory.push('/login');
 
-    if (isAuth)
-        return (
-            <RootWithSlideWidget
-                sidebar={<WidgetContainer/>}
-                open={isWidgetShown}
-                onSetOpen={setIsWidgetShown}
-                pullRight={true}
-                styles={{sidebar: {background: "white"}}}>
-                <div className={s.app}>
-                    <HeaderContainer setIsWidgetShown={setIsWidgetShown}/>
-                    <Sidebar/>
-                    <Content/>
-                </div>
-            </RootWithSlideWidget>
-        );
-    else return <Login/>
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {/* Don't forget to include the history module */}
+            <Router history={history}>
+                <header>
+                    <NavBar />
+                </header>
+                <Switch>
+                    <Route path="/" exact />
+                    <PrivateRoute path="/profile" component={Profile} />
+                </Switch>
+            </Router>
+        </div>
+    );
+
+    // if (isAuth)
+    //     return (
+    //         <RootWithSlideWidget
+    //             sidebar={<WidgetContainer/>}
+    //             open={isWidgetShown}
+    //             onSetOpen={setIsWidgetShown}
+    //             pullRight={true}
+    //             styles={{sidebar: {background: "white"}}}>
+    //             <div className={s.app}>
+    //                 <HeaderContainer setIsWidgetShown={setIsWidgetShown}/>
+    //                 <Sidebar/>
+    //                 <Content/>
+    //             </div>
+    //         </RootWithSlideWidget>
+    //     );
+    // else return <Login/>
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,30 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./redux/store";
+import {Auth0Provider} from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+const onRedirectCallback = appState => {
+    history.push(
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    );
+};
+
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App/>
+            <Auth0Provider
+                domain={config.domain}
+                client_id={config.clientId}
+                redirect_uri={window.location.origin}
+                onRedirectCallback={onRedirectCallback}
+            >
+                <App/>
+            </Auth0Provider>
         </Provider>
     </BrowserRouter>,
     document.getElementById('root'));
